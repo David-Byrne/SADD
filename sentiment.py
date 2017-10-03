dbd7c8de58f3bf82ae661c51094cff5da8d40e84
David Byrne <david.dbyrne@gmail.com>
2017-10-03 19:37:09 +0100

    Ties twitter connector and sentiment analyser together
    Now running the main function in `main.py` takes a user's twitter
    handle, fetches their last 300 tweets, and analyses them for
    positive or negative sentiment.

--- sentiment.py
+++ sentiment.py
@@ -1,50 +1,53 @@
 # See http://pythonforengineers.com/natural-language-processing-and-sentiment-analysis-with-python/
 
-import nltk.corpus
+import nltk
+from nltk.corpus import stopwords as nltk_stopwords
 from nltk.corpus import twitter_samples
 from nltk.classify import NaiveBayesClassifier
 
 
-STOP_WORDS = nltk.corpus.stopwords.words("english")
-BANNED_EMOJIS = [":)", ":(", ":-)", ":-(", ":D"]
-# NLTK seemed to gather positive and negative tweets just by looking for these emojis
-# so leaving them in causes the classifier to focus just on these rather than the text
+class SentimentClassifier(object):
 
+    STOP_WORDS = nltk_stopwords.words("english") + ["RT"]
+    BANNED_EMOJIS = [":)", ":(", ":-)", ":-(", ":D"]
+    # NLTK seemed to gather positive and negative tweets just by looking for these emojis
+    # so leaving them in causes the classifier to focus just on these rather than the text
 
-def is_useful_word(word):
-    if word in STOP_WORDS:
-        return False
-    if word.startswith(("@", "http://")):
-        return False
-    # If the word contains an illegal emoji, like ':)!!!'
-    if any(banned in word for banned in BANNED_EMOJIS):
-        return False
-    return True
+    def __init__(self):
+        neg_twts = [(self.create_word_features(twt), "negative")
+                    for twt in twitter_samples.strings('negative_tweets.json')]
 
+        pos_twts = [(self.create_word_features(twt), "positive")
+                    for twt in twitter_samples.strings('positive_tweets.json')]
 
-def create_word_features(tweet):
-    words = tweet.lower().split()
-    useful_words = filter(is_useful_word, words)
+        train_set = neg_twts[:4000] + pos_twts[:4000]
+        test_set = neg_twts[4000:] + pos_twts[4000:]
 
-    # Naive Bayes Classifier expects word:True pairs
-    formatted_words = dict([(word, True) for word in useful_words])
-    return formatted_words
+        self.classifier = NaiveBayesClassifier.train(train_set)
+        accuracy = nltk.classify.util.accuracy(self.classifier, test_set)
+        print("Initialised Classifier with accuracy of {}%".format(accuracy*100))
 
+    def is_useful_word(self, word):
+        if word in self.STOP_WORDS:
+            return False
+        if word.startswith(("@", "http://", "https://")):
+            return False
+        # If the word contains an illegal emoji, like ':)!!!'
+        if any(banned in word for banned in self.BANNED_EMOJIS):
+            return False
+        return True
 
-def main():
-    neg_twts = [(create_word_features(twt), "negative")
-                for twt in twitter_samples.strings('negative_tweets.json')]
+    def create_word_features(self, tweet):
+        words = tweet.lower().split()
+        useful_words = filter(self.is_useful_word, words)
 
-    pos_twts = [(create_word_features(twt), "positive")
-                for twt in twitter_samples.strings('positive_tweets.json')]
+        # Naive Bayes Classifier expects word:True pairs
+        formatted_words = dict([(word, True) for word in useful_words])
+        return formatted_words
 
-    train_set = neg_twts[:4000] + pos_twts[:4000]
-    test_set = neg_twts[4000:] + pos_twts[4000:]
-
-    classifier = NaiveBayesClassifier.train(train_set)
-    accuracy = nltk.classify.util.accuracy(classifier, test_set)
-    print(accuracy)
-
+    def classify(self, tweet):
+        word_features = self.create_word_features(tweet)
+        return self.classifier.classify(word_features)
 
 if __name__ == '__main__':
-    main()
+    classifier = SentimentClassifier()
