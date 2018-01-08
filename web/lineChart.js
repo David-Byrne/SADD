483957962db8ad21ceaf40f7e19cfee83c5e5ca7
David Byrne <david.dbyrne@gmail.com>
2018-01-08 19:27:09 +0000

    Adds sentiment line graph displaying mock data

---
+++ web/lineChart.js
@@ -0,0 +1,81 @@
+"use strict";
+
+class LineChart{
+
+    constructor(canvas, labels, saveData, repealData){
+        const chartConfig = {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: "#RepealThe8th",
+                    backgroundColor: 'rgb(0, 0, 0)',
+                    borderColor: 'rgb(0, 0, 0)',
+                    borderWidth: 2.5,
+                    pointRadius: 1,
+                    data: repealData,
+                    fill: false
+                }, {
+                    label: "#SaveThe8th",
+                    backgroundColor: 'rgb(237, 32, 123)',
+                    borderColor: 'rgb(237, 32, 123)',
+                    borderWidth: 2.5,
+                    pointRadius: 1,
+                    data: saveData,
+                    fill: false
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: "bottom"
+                },
+                title:{
+                    display:true,
+                    text:'Sentiment Chart [**FAKE DATA**]'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        ticks: {
+                            fontSize: 10
+                        },
+                        scaleLabel: {
+                            display: false,
+                            labelString: 'Date'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        ticks:{
+                            max: 1,
+                            min: 0
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Sentiment'
+                        }
+                    }]
+                }
+            }
+        };
+
+        this.chart = new Chart(canvas, chartConfig);
+    }
+
+    updateGraph(labels, saveData, repealData){
+        this.chart.config.data.labels = labels;
+        this.chart.config.data.datasets[0].data = repealData;
+        this.chart.config.data.datasets[1].data = saveData;
+        this.chart.update()
+    }
+}
