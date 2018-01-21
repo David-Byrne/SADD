12ca126a29218c94b5f44ce585517247615e3ca0
David Byrne <david.dbyrne@gmail.com>
2018-01-21 16:36:07 +0000

    Displays real data in wordclouds

--- web/sentiCloud.js
+++ web/sentiCloud.js
@@ -3,29 +3,30 @@
 /* global WordCloud:false */
 
 class SentiCloud { // eslint-disable-line no-unused-vars
-    constructor(element, data, extraConfig) {
+    constructor(element, extraConfig) {
         const defaultConfig = {
             fontFamily: 'Helvetica',
+            color: '#ffffff',
             rotateRatio: 0.0,
             rotationSteps: 2,
             shuffle: 0,
             wait: 10,
-            weightFactor: 2,
-            shape: 'square',
+            weightFactor: 1,
             gridSize: 12,
             classes: 'cloud-word',
         };
 
         this.config = Object.assign(defaultConfig, extraConfig);
-        this.config.list = data;
-        this.data = data;
+        this.config.list = [];
         this.element = element;
         WordCloud(element, this.config);
     }
 
     updateGraph(data) {
         // Maybe run a check first to see if the update would actually change things?
-        this.config.list = data;
+        data.sort((a, b) => b[1] - a[1]);
+        const scaledWords = data.map(entry => [entry[0], 6 * Math.log(entry[1] / 1.3)]);
+        this.config.list = scaledWords;
         WordCloud(this.element, this.config);
     }
 }
