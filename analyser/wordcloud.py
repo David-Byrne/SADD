3a2774d778372bfcbef051804aac80221da87072
David Byrne <david.dbyrne@gmail.com>
2018-01-27 19:17:48 +0000

    Uses twitter corpus to help determine term document frequency

--- analyser/wordcloud.py
+++ analyser/wordcloud.py
@@ -1,4 +1,6 @@
 import operator
+from collections import Counter
+from nltk.corpus import twitter_samples
 
 
 class WordCloud(object):
@@ -14,10 +16,40 @@ class WordCloud(object):
     def calc_relative_word_freq(words, contrast):
         scores = {}
         for word, count in words.items():
-            scores[word] = count / (contrast[word] + 1)
+            idf = 1 + contrast[word] + (0.1 * _normal_word_freq[word])
+            scores[word] = count / idf
         return scores
 
     @staticmethod
     def get_top_words(words):
         sorted_words = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
         return sorted_words[:100]
+
+    @staticmethod
+    def parse_tweet(text):
+        words = text.lower() \
+            .replace(".", " ") \
+            .replace(",", " ") \
+            .replace(";", " ") \
+            .replace(":", " ") \
+            .replace('"', " ") \
+            .replace("“", " ") \
+            .replace("”", " ") \
+            .replace("!", " ") \
+            .replace("?", " ") \
+            .replace("-", " ") \
+            .replace("*", " ") \
+            .replace("(", " ") \
+            .replace(")", " ") \
+            .replace("<", "&lt;") \
+            .replace(">", "&gt;") \
+            .split()
+        words_wo_links = [w for w in words if not w.startswith("co/")]
+        #  ^This removes Twitter shorthand URLs
+        words_wo_quots = [w.strip("'‘’") for w in words_wo_links]
+        return [w for w in words_wo_quots if len(w) > 1]
+
+
+_normal_word_freq = Counter()
+[_normal_word_freq.update(WordCloud.parse_tweet(twt)) for twt in
+ twitter_samples.strings("tweets.20150430-223406.json")]
